d0eb77a50491b423f96d98a1052d71b079761332
Kenny Cruz <kennyandes@gmail.com>
2023-12-13 15:57:16 -0500

    fix service

--- src/app/services/aws.service.ts
+++ src/app/services/aws.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { S3 } from 'aws-sdk';
+import { S3  } from 'aws-sdk';
 import { environment } from 'environments/environment';
+import { Observable, from, map,catchError,throwError,pipe } from 'rxjs';
+import { ListObjectsV2Command,S3Client } from '@aws-sdk/client-s3';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,17 +14,26 @@ export class AwsService {
     secretAccessKey: 'IOSrFQlkwXXC87iJP4k+WUV9xqBDGDrXCF6e8PHW',
   };
  
+  client = new S3Client({});
+
+
   constructor() {
     this.s3Client = new S3({
       region: this.REGION,
       credentials: this.credentials,
     });   }
 
-   getObjeto(key: string): Promise<any> {
-    return this.s3Client.getObject({ Bucket: environment.aws.bucket, Key: key }).promise()
-      .then(data => data.Body)
-      .catch(error => console.error(error));
+  //  getObjeto(key: string): Observable<any> {
+  //   return this.s3Client.getObject({ Bucket: environment.aws.bucket, Key: key }).promise()
+  //   .then(data => {
+  //     return data.Body['data'];
+  //   })      .catch(error => console.error(error));
+  // }
+
+  getObjeto(key: string): Observable<any> {
+    return from(this.s3Client.getObject({ Bucket: environment.aws.bucket, Key: key }).promise());
   }
+
 
   subirImagen(imagen: File): Promise<void> {
     const key = `imagen-${Date.now()}.jpg`;
@@ -36,4 +47,51 @@ export class AwsService {
       .then(() => console.log('Imagen subida'))
       .catch(error => console.error(error));
   }
+
+  listBucketObjects(): Promise<S3.Object[]> {
+    const command = new ListObjectsV2Command({
+      Bucket: environment.aws.bucket,
+      MaxKeys: 1000,
+    });
+    let  a = this.client.send(command)
+    .then(response => {
+      console.log("Response:", response); // Added this line
+      const objects = response.Contents;
+      let isTruncated = response.IsTruncated;
+      let nextToken = response.NextContinuationToken;
+      return objects;
+    });
+    console.log("aaa: " + JSON.stringify(a))
+    return this.client.send(command)
+      .then(response => {
+        const objects = response.Contents;
+        let isTruncated = response.IsTruncated;
+        let nextToken = response.NextContinuationToken;
+        return objects;
+      });
+  }
+
+  async listBucketObjects1(bucketName: string): Promise<string[]> {
+    const command = new ListObjectsV2Command({
+      Bucket: bucketName,
+      MaxKeys: 1000, // Adjust as needed
+    });
+  
+    let isTruncated = true;
+    let contents = "";
+  
+    try {
+      while (isTruncated) {
+        const { Contents, IsTruncated, NextContinuationToken } = await this.client.send(command);
+        const contentsList = Contents.map((c) => c.Key); // Extract just the keys
+        contents += contentsList.join("\n") + "\n";
+        isTruncated = IsTruncated;
+        command.input.ContinuationToken = NextContinuationToken;
+      }
+      return contents.trim().split("\n"); // Return an array of object keys
+    } catch (err) {
+      throw err;
+    }
+  }
 }
+
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,39 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { AwsService } from 'app/services/aws.service';
+import { Component, OnInit } from "@angular/core";
+import { AwsService } from "app/services/aws.service";
+import { S3 } from "aws-sdk";
+import { ListObjectsV2Command,S3Client } from '@aws-sdk/client-s3';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"],
 })
 export class UserComponent implements OnInit {
   selectedValues: any;
   value3: any;
   imageUrl: any;
-  constructor(private s3Service: AwsService) { }
+  constructor(private s3Service: AwsService,private s3: S3Client) {}
 
-  ngOnInit() {
-    this.s3Service.getObjeto('imagen-1702435537925.jpg').then(
-      (data) => {
-        this.imageUrl = btoa(data.Body);
-        console.log("objeto: " + JSON.stringify( this.imageUrl));
-      },
-      (error) => {
-        console.error(error);
-      });
-  }
-
-  subirImagen() {
-    const imagen = document.getElementById('imagen') as HTMLInputElement;
-    const file = imagen.files[0];
-    try{
-    this.s3Service.subirImagen(file).then(
-      () => console.log('Imagen subida'),
-      error => console.error(error)
-    );
-    }catch(error){
-console.log("error: " +error)
+  async ngOnInit() {
+    // this.s3Service.getObjeto("imagen-1702435537925.jpg").subscribe(
+    //   (data) => {
+    //     this.imageUrl = data.Body.toString("base64");
+    //   },
+    //   (error) => {
+    //     console.error(error);
+    //   }
+    // );
+    
+    const objects = await this.s3Service.listBucketObjects1('awsunibe');
+    for (const object of objects) {
+      console.log(object);
     }
   }
 
+  subirImagen() {
+    const imagen = document.getElementById("imagen") as HTMLInputElement;
+    const file = imagen.files[0];
+    try {
+      this.s3Service.subirImagen(file).then(
+        () => console.log("Imagen subida"),
+        (error) => console.error(error)
+      );
+    } catch (error) {
+      console.log("error: " + error);
+    }
+  }
 }
