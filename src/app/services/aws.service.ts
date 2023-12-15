941927dcab5ae438a5b687247404edb4f58103c7
kencruga <kennyandes@gmail.com>
2023-12-14 22:40:30 -0500

    implement uploadfile

--- src/app/services/aws.service.ts
+++ src/app/services/aws.service.ts
@@ -1,57 +1,65 @@
 import { Injectable } from '@angular/core';
-// import { S3  } from 'aws-sdk';
 import { environment } from 'environments/environment';
 import { Observable, from, map,catchError,throwError,pipe } from 'rxjs';
-import { ListObjectsV2Command } from '@aws-sdk/client-s3';
-import { S3 } from '@aws-sdk/client-s3';
+import { S3,GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
+const contentTypeMap = {
+  'image/jpeg': 'image/jpeg',
+  'image/png': 'image/png',
+  'application/pdf': 'application/pdf',
+};
 @Injectable({
   providedIn: 'root'
 })
 export class AwsService {
-  //private s3Client: S3;
-  // objects: S3.ListObjectsOutput;
-  // REGION = 'us-east-2';
-  // credentials = {
-  //   accessKeyId: 'AKIATHGZBEJMMY44HIP2',
-  //   secretAccessKey: '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',
-  // };
+  private s3Client: S3;
+  REGION = 'us-east-2';
+  credentials = {
+    accessKeyId: 'AKIATHGZBEJMMY44HIP2',
+    secretAccessKey: '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',
+  };
  
-  // client = new S3Client({});
 
+  constructor(private s3: S3) {
+    this.s3Client = new S3({
+      region: this.REGION,
+      credentials: this.credentials,
+    });   }
 
-  // constructor(private s3: S3) {
-  //   this.s3Client = new S3({
-  //     region: this.REGION,
-  //     credentials: this.credentials,
-  //   });   }
+    
+    getObjeto(key: string): Promise<any> {
+      return this.s3Client.getObject({ Bucket: environment.aws.bucket, Key: key })
+        .then(data => {
+        console.log("Dataservoe: " + JSON.stringify(data))
+          data.Body
+        })
+        .catch(error => console.error(error));
+    }
 
-  //  getObjeto(key: string): Observable<any> {
-  //   return this.s3Client.getObject({ Bucket: environment.aws.bucket, Key: key }).promise()
-  //   .then(data => {
-  //     return data.Body['data'];
-  //   })      .catch(error => console.error(error));
-  // }
+    // async getObject(objectKey: string) {
+    //   const params = {
+    //     Bucket: environment.aws.bucket,
+    //     Key: objectKey,
+    //   };
+    //   const data = await this.s3C.getObject(params);
+    //   console.log("data: " +JSON.stringify(data))
+    //   return data.Body.toString();
+    // }
 
-  // getObjeto(key: string): Observable<any> {
-  //   return from(this.s3Client.getObject({ Bucket: environment.aws.bucket, Key: key }).promise());
-  // }
+   subirArchivo(archivo: File): Promise<void> {
+    const filename = archivo.name;
+    const key = `${filename}`;
+    const contentType = contentTypeMap[archivo.type];
+    const body = new Blob([archivo], { type: archivo.type });
 
-
-  // subirImagen(imagen: File): Promise<void> {
-  //   const key = `imagen-${Date.now()}.jpg`;
-  //   const body = new Blob([imagen], { type: 'image/jpg' });
-
-  //   return this.s3Client.putObject({
-  //     Bucket: environment.aws.bucket,
-  //     Key: key,
-  //     Body: body
-  //   }).promise()
-  //     .then(() => console.log('Imagen subida'))
-  //     .catch(error => console.error(error));
-  // }
-
-
+    return this.s3Client.putObject({
+      Bucket: environment.aws.bucket,
+      Key: key,
+      Body: body,
+      ContentType: contentType,
+    }).then(() => console.log('Imagen subida'))
+        .catch(error => console.error(error));
+  }
 
 }
 
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { S3 } from '@aws-sdk/client-s3';
-import { from,map } from "rxjs";
+import { S3  } from '@aws-sdk/client-s3';
 import { environment } from "environments/environment";
+import { AwsService } from "app/services/aws.service";
 @Component({
   selector: "app-user",
   templateUrl: "./user.component.html",
@@ -9,8 +9,13 @@ import { environment } from "environments/environment";
 })
 export class UserComponent implements OnInit {
   objects: any;
+  imageUrl: any;
+  objectContent: string | null;
+  params = {
+    Bucket: environment.aws.bucket,
+  };
 
-  constructor(private s3: S3) {
+  constructor(private s3: S3, private awsService: AwsService) {
     this.s3 = new S3({
       credentials: {
         accessKeyId: environment.aws.accessKeyId,
@@ -20,15 +25,51 @@ export class UserComponent implements OnInit {
     });
   }
 
-  async ngOnInit() {
-    const params = {
-      Bucket: environment.aws.bucket,
-    };
+   ngOnInit() {
+   this.listarObjetos();
+this.obtenerArhivo();
+    //  this.awsService.getObjeto("imagen-1702435537925.jpg").subscribe(
+    //   (data) => {
+    //     console.log("Data: " + JSON.stringify(data))
+    //     this.imageUrl = data.Body.toString("base64");
+    //   },
+    //   (error) => {
+    //     console.error(error);
+    //   }
+    // );
 
-    this.s3.listObjectsV2(params).then(response => {
-      console.log("ASDSA then: " + JSON.stringify(response.Contents));
+  }
+
+  obtenerArhivo(){
+
+    this.awsService
+      .getObjeto('imagen-1702435537925.jpg')
+      .then((content) => {
+        console.log("content: " + JSON.stringify(content))
+        this.objectContent = content.Body.toString("base64");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  listarObjetos(){
+    this.s3.listObjectsV2(this.params).then(response => {
       this.objects = response.Contents;
     });
+  }
+
+  subirArchivo() {
+    const archivo = document.getElementById("archivo") as HTMLInputElement;
+    const file = archivo.files[0];
+    try {
+      this.awsService.subirArchivo(file).then(
+        () => console.log("archivo subido"),
+        (error) => console.error(error)
+      );
+    } catch (error) {
+      console.log("error: " + error);
+    }
   }
 
 }
